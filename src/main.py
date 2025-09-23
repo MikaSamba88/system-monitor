f353736cb14822de65136635f1f07dc5cc5706ea
MikaSamba88 <mika.samba@chasacademy.se>
2025-09-23 11:09:23 +0200

    Update main menu to include options and improve formatting

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
 def main_menu():
     while True:
-        print("-----------------------------")
-        print("\n--- Systemövervakning ---")
+        print("-------------------------")
+        print("--- Systemövervakning ---")
+        print("\n1. Starta övervakning")
+        
+        choice = input("Välj ett alternativ i Menyn (1-6)")
+        break
+if __name__ == "__main__":
+    main_menu()
