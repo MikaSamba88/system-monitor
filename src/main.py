7ba14077d7325c40a176c0f88bd78722c18edd82
MikaSamba88 <mika.samba@chasacademy.se>
2025-09-25 14:28:35 +0200

    Add psutil import to main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
 def main_menu():
     while True:
         print("-------------------------")
-        print("--- Systemövervakning ---")
-        print("\n1. Starta övervakning")
+        print("--- Systemoverview ---")
+        print("\n1. Starta overview")
+        print("2. Check Alarms")
+        print("3. Check Alarms")
+        print("4. Check Alarms")
+        print("5. Check Alarms")
+        print("6. Check Alarms")
         
-        choice = input("Välj ett alternativ i Menyn (1-6)")
+        choice = input("\nVälj ett alternativ i Menyn (1-6)")
+
+        if choice == "1":
+            print("Start Overview! skall lägga in funktion")
         break
 if __name__ == "__main__":
     main_menu()
