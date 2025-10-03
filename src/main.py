e542fd1ac91aaf6f124e536d391aad27f5db8ead
MikaSamba88 <mika.samba@chasacademy.se>
2025-10-03 09:39:41 +0200

    ds store ignore

--- src/main.py
+++ src/main.py
@@ -1,18 +1,53 @@
+import psutil, os, logging, time, threading, json, prettytable
+from simple_term_menu import TerminalMenu
+
+
+def print_menu():
+    print("-" * 24)
+    print("---- Systemoverview ----")
+    print("-" * 24)
+    print("\n1. System overview")
+    print("2. Start Surveillance")
+    print("3. Check Alarms")
+    print("4. Remove Alarms ")
+    print("5. Check Alarms")
+    print("6. ")
+
+#def list_alarms()
+
+def exit_program():
+    clear_consol()
+    print("You choose to exit!\n")
+    print(input("Tryck Enter för att avsluta..."))
+
 def main_menu():
+    options = ["entry 1", "entry 2", "entry 3"]
+    terminal_menu = TerminalMenu(options)
+    menu_entry_index = terminal_menu.show()
+    print(f"You have selected {options[menu_entry_index]}!")
+    alarms = []
     while True:
-        print("-------------------------")
-        print("--- Systemoverview ---")
-        print("\n1. Starta overview")
-        print("2. Check Alarms")
-        print("3. Check Alarms")
-        print("4. Check Alarms")
-        print("5. Check Alarms")
-        print("6. Check Alarms")
-        
+        print_menu()
+        choice = input("Choose a number(1-5): ")
+        match choice:
+            case "1":
+                pass
+            case "2":
+                pass
+            case "3":
+                pass
+            case "4":
+                pass
+            case "5":
+                pass
+        exit_program()
+        sys.exit()
         choice = input("\nVälj ett alternativ i Menyn (1-6)")
 
         if choice == "1":
             print("Start Overview! skall lägga in funktion")
+
+
         break
 if __name__ == "__main__":
     main_menu()
---
+++ src/storage.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+import json
+
+DATA_PATH = Path("Alarms.json")
+
+
+def save(alarms):
+    with open(DATA_PATH, "w") as file:
+        json.dump(alarms, file, indent=4)
+
+
+def load():
+    if not DATA_PATH.exists():
+        return []
+
