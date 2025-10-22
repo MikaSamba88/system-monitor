5dc3655d5a79f2db2ea1ab79666d23e867a43fe7
MikaSamba88 <mika.samba@chasacademy.se>
2025-10-22 14:58:48 +0200

    Add alarm functionality to monitoring mode for CPU, memory, and disk usage

--- src/monitoring.py
+++ src/monitoring.py
@@ -1,6 +1,8 @@
-import psutil, os, time
+import psutil, os, time, threading
 from prettytable import PrettyTable
-from functions import display_system_status
+from functions import *
+from alarms import *
+
 
 class Monitor:
     monitoring_active = False
@@ -21,11 +23,12 @@ class Monitor:
 
         if not cls.monitoring_active:
             print("No monitoring is active.")
+            input("\nPress Enter to continue...")
         else:
             display_system_status()
 
     @classmethod
-    def monitoring_mode(cls):
+    def monitoring_mode(cls, manager):
         if not cls.monitoring_active:
             print("Cannot start monitoring mode. No monitoring Active.")
             input("\nPress Enter to return to main...")
@@ -38,7 +41,25 @@ class Monitor:
                 memory = psutil.virtual_memory().percent
                 disk = psutil.disk_usage('/').percent
                 print(f"CPU: {cpu}% | Memory: {memory}% | Disk: {disk}%")
-                time.sleep(2)
+                time.sleep(1)
+
+                for level in manager.alarms['cpu']:
+                    if cpu >= level:
+                        alarm_sound()
+                        print(f"🚨 Warning! CPU is using {cpu}%, Alarm set at {level} 🚨")
+
+                for level in manager.alarms['memory']:
+                    if memory >= level:
+                        alarm_sound()
+                        print(f"🚨 Warning! Memory is using {memory}%, Alarm set at {level} 🚨")
+
+                for level in manager.alarms['disk']:
+                    if disk >= level:
+                        alarm_sound()
+                        print(f"🚨 Warning! Disk is using {disk}%, Alarm set at {level} 🚨")
+                time.sleep(1)
+                clear_consol()
+
         except KeyboardInterrupt:
             print("\nMonitoring mode stopped. Returing to main menu...")
             time.sleep(1)
